fa0df591b4640299e3b5ff662f5e6eec9ab20700
artryan130 <80287857+artryan130@users.noreply.github.com>
2022-05-06 17:30:50 -0300

    Routes

--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-import React from 'react';
 import { render } from 'react-dom';
-import { App } from '../imports/ui/App';
-import {renderRoutes} from '../imports/ui/routes'
+import { renderRoutes } from '../imports/ui/routes'
 
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('react-target'));
